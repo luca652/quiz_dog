d1dbf02e1a5ccba18bfece2b9548b24825ec0933
luca gastaldi <lucagastaldi82@gmail.com>
2023-09-05 13:43:23 +0100

    created join table gigs between quizmasters and venues

---
+++ db/migrate/20230905124120_create_gigs.rb
@@ -0,0 +1,10 @@
+class CreateGigs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :gigs do |t|
+      t.references :venue, null: false, foreign_key: true
+      t.references :quizmaster, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
