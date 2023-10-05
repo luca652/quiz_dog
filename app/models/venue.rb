ee852ac1e80bda6d376810bae225ffb8b61eb7da
luca gastaldi <lucagastaldi82@gmail.com>
2023-10-05 15:37:00 +0100

    styled quizmaster cards

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,7 +1,7 @@
 class Venue < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  # validates :name, :address, :phone_number, presence: true
+
   belongs_to :user
   belongs_to :quizmaster
   has_one_attached :photo
