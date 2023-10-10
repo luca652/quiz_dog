0135344b9d1bfa06896e3889c9f79836f7b88f3d
luca gastaldi <lucagastaldi82@gmail.com>
2023-10-10 16:33:44 +0100

    created multiple_images_controller.js in javascript/controllers, added two more cards to carousel

---
+++ app/javascript/controllers/multiple_images_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  static targets = ['card1', 'card2', 'card3', 'card4', 'card5']
+
+  connect() {
+    console.log("Connected!");
+    let elements = [this.card1Target, this.card2Target, this.card3Target];
+    console.log(elements);
+
+    elements.forEach((el) => {
+      const minPerSlide = 4
+      let next = el.nextElementSibling
+      for (var i=1; i<minPerSlide; i++) {
+          if (!next) {
+              // wrap carousel by using first child
+            next = elements[0]
+          }
+          let cloneChild = next.cloneNode(true)
+          el.appendChild(cloneChild.children[0])
+          next = next.nextElementSibling
+      }
+  })
+  }
+}
