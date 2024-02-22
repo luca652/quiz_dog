d9c58d76f6e8e0f687bc51697f0bf334dbdae477
luca gastaldi <lucagastaldi82@gmail.com>
2024-02-22 11:23:49 +0000

    added validations to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :number, presence: true
+  validates :message, presence: true
 end
