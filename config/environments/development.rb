c83d2a02ac09931380fcbb6df9963aa877cbe4b1
luca gastaldi <lucagastaldi82@gmail.com>
2024-02-22 14:29:57 +0000

    added gmail address and password to credentials - configured mailer for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,18 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => Rails.application.credentials.quiz_dog_mailer.address,
+    :password             => Rails.application.credentials.quiz_dog_mailer.password,
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
