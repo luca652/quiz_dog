d3e31f9dab901708704cec5743b402474096e280
luca gastaldi <lucagastaldi82@gmail.com>
2024-08-20 17:13:10 +0100

    Change delivery address for mailer Added email address to env file and referenced it as env variable in the code.

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -4,7 +4,7 @@ class ContactUsMailer < ApplicationMailer
 
   def new_question
     @message = params[:message]
-    mail(to: "lucagastaldi82@gmail.com", subject: "New Message from Quiz Dog website")
+    mail(to: ENV["MAILER_DELIVERY_ADDRESS"], subject: "New Message from Quiz Dog website")
   end
 
 end
