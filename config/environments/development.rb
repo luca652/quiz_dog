cddbc5a4b28580ea80e3143af9cf194c042dd42d
luca gastaldi <lucagastaldi82@gmail.com>
2024-08-20 17:04:58 +0100

    Set up mailer to workt with Zoho email address The mailer sends the message to the email address selected in the backend.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,20 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 
-  # config.action_mailer.smtp_settings = {
-  #   :address              => "smtp.gmail.com",
-  #   :port                 => 587,
-  #   :user_name            => Rails.application.credentials.quiz_dog_mailer.address,
-  #   :password             => Rails.application.credentials.quiz_dog_mailer.password,
-  #   :authentication       => "plain",
-  #   :enable_starttls_auto => true
-  # }
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.zoho.com",
+  :port                 => 587,
+  :user_name            => Rails.application.credentials.quiz_dog_mailer.address,
+  :password             => Rails.application.credentials.quiz_dog_mailer.password,
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+  # config.action_mailer.perform_deliveries = true
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
