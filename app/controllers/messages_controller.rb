3729e7625a64f3645af3195f49c83f0e9c331cfe
luca gastaldi <lucagastaldi82@gmail.com>
2024-08-21 09:31:05 +0100

    Validations Add validations for presence for all attributes Add validations for length for all attributes Add validation that checks that the email address is a formatted properly Add validation that checks that number is a number

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
       ContactUsMailer.with(message: @message).new_question.deliver_now
       redirect_to contact_us_path, notice: "Thank you for your message!"
     else
-      render :new
+      render 'pages/contact_us', status: :unprocessable_entity
     end
   end
 
